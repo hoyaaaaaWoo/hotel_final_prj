131c8842a67296dd2f7d8b92c9753daebd271075
user <user@DESKTOP-COJ9858>
2021-12-11 12:23:22 +0900

    ddd

--- hotel_final_prj/src/main/java/kr/co/mvc/admin/controller/LoginContoller.java
+++ hotel_final_prj/src/main/java/kr/co/mvc/admin/controller/LoginContoller.java
@@ -1,5 +1,33 @@
 package kr.co.mvc.admin.controller;
 
-public class LoginContoller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import kr.co.mvc.admin.service.LoginService;
+import kr.co.mvc.admin.vo.ManagerVO;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;;
+@Controller
+@SessionAttributes("id")
+public class LoginContoller {
+	
+	private LoginService lSev;
+	
+@RequestMapping(value="admin_login_form.do", method = POST)
+public String moveAdminLoginForm() {
+		return"";
+	}//moveAdminLoginForm
+@RequestMapping(value="admin_login.do")
+public String adminLogin(ManagerVO mVO) {
+	return"";
 }
+@RequestMapping(value="admin_logout.do")
+public String adminLogOut(SessionStatus ss) {
+	return "";
+}
+	
+}//class
--- hotel_final_prj/src/main/java/kr/co/mvc/admin/controller/MemberController.java
+++ hotel_final_prj/src/main/java/kr/co/mvc/admin/controller/MemberController.java
@@ -1,5 +1,26 @@
 package kr.co.mvc.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.mvc.admin.service.MemberService;
+
+@Controller
 public class MemberController {
 
+	private MemberService mSev;
+@RequestMapping(value="search_member.do")
+public String searchMember(String id, Model model) {
+	return "";
 }
+@RequestMapping(value="search_deleteMember.do")
+public String searchDeleteMember(String id,Model model) {
+	return"";
+}
+@RequestMapping(value="change_member.do")
+public String changeMember(String id,Model model) {
+	return"";
+}
+}//class
+
